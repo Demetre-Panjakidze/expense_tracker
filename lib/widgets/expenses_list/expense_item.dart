3e3a46b1255765a8df00020f7bd25170e858db3a
Demetre <demetrepanjakidze1@gmail.com>
2023-05-23 16:01:50 +0400

    progress!

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(expense.title),
+            ),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 8.0),
+                      child: Icon(Icons.alarm),
+                    ),
+                    Text(expense.date.toString()),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
